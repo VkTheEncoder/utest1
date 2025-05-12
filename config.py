24420b755b4657da68d75ab15390314616a60770
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-05-12 19:23:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ API_BASE = os.getenv("ANIWATCH_API_BASE", "http://localhost:4000/api/v2/hianime"
 
 if not API_ID or not API_HASH:
     raise RuntimeError("API_ID and API_HASH must be set in .env")
+STATE = {}
