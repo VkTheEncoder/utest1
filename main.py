36a70bc271cd75298811b3719d2eef7e8eaff849
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-05-12 18:41:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-# main.py
-import logging
 from telethon import TelegramClient
 from config import API_ID, API_HASH
 from handlers import register_handlers
-
-logging.basicConfig(level=logging.INFO)
+import asyncio
 
 client = TelegramClient('hianime_session', API_ID, API_HASH)
 
 async def main():
     await client.start()
     await register_handlers(client)
-    print("🚀 Bot is up!")
+    print("🚀 Bot is up and running")
     await client.run_until_disconnected()
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
